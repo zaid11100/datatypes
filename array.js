49f7e54226c34dfddf3e5ea787d03abce58a565c
zaid11100 <info.zaydali@gmail.com>
2022-07-23 11:35:32 +0530

    hoisitng in js

---
+++ array.js
@@ -0,0 +1,64 @@
+//Represented by [..... ]
+//collection of elements/items
+//declare as many as possible
+//any data typeis possible 
+
+// a=[1,2,"f",5,]
+// //length is 4
+// console.log(a[0+3])
+// console.log(a.length)
+
+
+
+// let a=['a','b','c','d']
+// console.log(a,typeof a)
+
+//Indexing
+
+// console.log(a[0])
+
+// console.log(a[1])
+
+// console.log(a[2])
+
+// console.log(a[3])
+
+//SLICING
+// console.log(a.slice())
+
+// console.log(a.slice(2))
+// console.log(a.slice(3))
+// console.log(a.slice(0,3))
+
+// let a="['a','b','c','d','e','f']"
+// console.log(reverse(a))
+// let a=['a',['b','c'],['dell','eat','f']]
+// console.log(a[1])
+// b=a[2][0]
+// console.log(a[2][0][2])
+// console.log(a.reverse)
+
+// a = [1,2,3,4]
+// console.log(a)
+// a.push(7)
+// a.push(6)
+// a.push(5)
+
+// a.pop(3,4)
+// a.unshift(0)
+// console.log(a)
+// a.shift(0)
+// console.log(a)
+
+//SPLICE
+
+// a=[1,2,3,4,5,6,8,9]
+// a.splice(1)
+// a.splice(2)
+// a.splice(3)
+// a.splice(4)
+// a.splice(5)
+// a.splice(6)
+// a.splice(7)
+// console.log(a)
+
